da05d8ca0e85fb19cb37a7cf2d2550baf9a1adba
justhis01010101 <offentsive@naver.com>
2017-04-11 20:50:45 +0900

    두번째 프레임

---
+++ app/src/main/java/youn0045/kr/hs/emirim/frametest/Linearlayout.java
@@ -0,0 +1,8 @@
+package youn0045.kr.hs.emirim.frametest;
+
+/**
+ * Created by appcreator16 on 2017. 4. 11..
+ */
+
+class Linearlayout {
+}
--- app/src/main/java/youn0045/kr/hs/emirim/frametest/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/frametest/MainActivity.java
@@ -2,13 +2,46 @@ package youn0045.kr.hs.emirim.frametest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout[] linears= new LinearLayout[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button[] buts = new Button[3];
 
+        for (int i = 0; i < buts.length; i++) {
+            buts[i] = (Button) findViewById(R.id.but1 + i); //
+            linears[i]= (LinearLayout)findViewById(R.id.line1+1);
+            buts[i].setOnClickListener(butHandler); //
+        }
     }
+
+    View.OnClickListener butHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) { //버튼을 클릭했을때 호출되는 부분
+            switch (view.getId()){
+                case R.id.but1:
+                    linears[0].setVisibility(View.VISIBLE);
+                    linears[1].setVisibility(View.INVISIBLE);
+                    linears[2].setVisibility(View.INVISIBLE);
+                    break;
+                case R.id.but2:
+                    linears[0].setVisibility(View.INVISIBLE);
+                    linears[1].setVisibility(View.VISIBLE);
+                    linears[2].setVisibility(View.INVISIBLE);
+                    break;
+                case R.id.but3:
+                    linears[0].setVisibility(View.INVISIBLE);
+                    linears[1].setVisibility(View.INVISIBLE);
+                    linears[2].setVisibility(View.VISIBLE);
+                    break;
+            }
+
+        }
+    };
 }
